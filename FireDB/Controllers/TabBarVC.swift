4f7744a5659d14b0d206aca2791a67f75f4a5473
Abhishek Sharma <abhishek.sharma@particle41.com>
2019-09-11 15:47:26 +0530

    added custom camera on sell new item button on bottom tab bar.

--- FireDB/Controllers/TabBarVC.swift
+++ FireDB/Controllers/TabBarVC.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class TabBarVC: UITabBarController {
-
+    @IBOutlet weak var btnSell: UIButton!
+    
+    var btnWidth = CGFloat(60.0)
+    var btnHeightOverTab = CGFloat(-15.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .medium)], for: .normal)
-        
+        self.view.insertSubview(self.btnSell, aboveSubview: self.tabBar)
+        btnSell.layer.shadowColor = btnSell.backgroundColor?.cgColor
+        btnSell.layer.masksToBounds = false
+        btnSell.layer.shadowOffset = CGSize(width: 1, height: 1)
+        btnSell.layer.shadowRadius = 1.5
+        btnSell.layer.shadowOpacity = 0.5
+        self.btnSell.addTarget(self, action: #selector(btnSellAction(_:)), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.btnSell.frame = CGRect.init(x: self.tabBar.center.x - (btnWidth/2.0), y: self.view.bounds.height - (self.tabBar.frame.height - self.btnHeightOverTab), width: btnWidth, height: btnWidth)
+        self.btnSell.layer.cornerRadius = btnWidth/2.0
+    }
+    
+    @IBAction func btnSellAction(_ sender : UIButton) {
+        if self.selectedIndex != 2 {
+            if (self.viewControllers?.count ?? 0) > 2, let navVC = self.viewControllers?[2] as? UINavigationController {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddSellItemVC")
+                navVC.setViewControllers([vc!], animated: false)
+            }
+            self.selectedIndex = 2
+        }
+    }
     /*
     // MARK: - Navigation
 
